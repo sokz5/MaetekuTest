1f2c7389ac626d0aade7fa57a51ae799356f8f51
sokz05 <main@sokz.net>
2020-10-01 14:10:36 +0900

    Initial Commit

---
+++ MaetekuTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MaetekuTest
+//
+//  Created by 井口創太 on 2020/10/01.
+//  Copyright © 2020 sokz05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
