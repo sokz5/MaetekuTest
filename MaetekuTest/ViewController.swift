72df12afdcb7d733aef2a2e393cc59bf78354c93
sokz05 <main@sokz.net>
2020-10-25 18:27:39 +0900

    v0.0.1 テスト用

--- MaetekuTest/ViewController.swift
+++ MaetekuTest/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button1(_ sender: Any) {
+        label1.text = "Hello, World!"
+        
+    }
+    
 }
 
